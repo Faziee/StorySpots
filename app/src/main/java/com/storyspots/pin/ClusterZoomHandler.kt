b63c2dd5c81cb19f3a62f03359e839af0e2e527c
Josephine Stensgaard <josephine.stensgaard@student.nhlstenden.com>
2025-05-30 17:14:28 +0200

    zoom works, but not ideally

--- app/src/main/java/com/storyspots/pin/ClusterZoomHandler.kt
+++ app/src/main/java/com/storyspots/pin/ClusterZoomHandler.kt
@@ -1,8 +1,8 @@
 package com.storyspots.pin
 
 import android.util.Log
-import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.plugin.animation.MapAnimationOptions
 import com.mapbox.maps.plugin.animation.camera
 import com.mapbox.maps.plugin.gestures.addOnMapClickListener
@@ -13,30 +13,28 @@ class ClusterZoomHandler {
         private const val TAG = "ClusterZoomHandler"
 
         fun setupClusterClickHandler(mapView: MapView, sourceId: String) {
-            Log.d(TAG, "Setting up cluster-specific click handler")
+            Log.d(TAG, "Setting up cluster click handler - always zoom on click")
 
-            mapView.mapboxMap.addOnMapClickListener { clickPoint ->
+            mapView.mapboxMap.addOnMapClickListener { point ->
                 val currentZoom = mapView.mapboxMap.cameraState.zoom
 
-                if (currentZoom < 12.0) {
-                    Log.d(TAG, "Low zoom level ($currentZoom), assuming cluster click - zooming in")
-
-                    val newZoom = currentZoom + 3.0
+                if (currentZoom < 16.0) {  // Zoom in until level 16
+                    Log.d(TAG, "Zoom $currentZoom - zooming in to break clusters")
 
                     mapView.camera.easeTo(
                         CameraOptions.Builder()
-                            .center(clickPoint)
-                            .zoom(newZoom)
+                            .center(point)
+                            .zoom(currentZoom + 2.0)
                             .build(),
                         MapAnimationOptions.mapAnimationOptions {
                             duration(500)
                         }
                     )
 
-                    return@addOnMapClickListener true
+                    true // Consume event
                 } else {
-                    Log.d(TAG, "High zoom level ($currentZoom), letting individual pin clicks work")
-                    return@addOnMapClickListener false
+                    Log.d(TAG, "Max zoom reached - allowing pin creation")
+                    false // Allow pin creation
                 }
             }
         }
