b44f2ede2f571089773b7cbdaf65e08f7a3bf575
blossomAnukposi <blossom.anukposi1@student.nhlstenden.com>
2025-06-01 23:02:47 +0200

    Made Application class

---
+++ app/src/main/java/com/storyspots/StorySpot.kt
@@ -0,0 +1,18 @@
+package com.storyspots
+
+import android.app.Application
+
+class StorySpot : Application()
+{
+    //Initialise Database
+
+    //Set up Network
+
+    //SDK Initialisations
+
+    //Global Configurations
+
+    //Shared Preferences Setup
+
+    //Background services
+}
