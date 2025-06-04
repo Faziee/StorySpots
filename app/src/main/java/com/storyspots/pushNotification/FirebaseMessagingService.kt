d8b73c8180a807bb3446e7f0154825ebedf072c4
Jo-GC <jordana.guilbride.capela@student.nhlstenden.com>
2025-06-04 21:34:53 +0200

    Add: Push Notification base

---
+++ app/src/main/java/com/storyspots/pushNotification/FirebaseMessagingService.kt
@@ -0,0 +1,41 @@
+package com.storyspots.pushNotification
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.storyspots.R
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class FirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage){
+        remoteMessage.notification?.let {
+            showNotification(it.title, it.body)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("FCM", "New token: $token")
+    }
+
+    private fun showNotification(title: String?, body: String?){
+        val channelId ="default_channel_id"
+        val notificationBuilder = NotificationCompat.Builder(this, channelId).setContentTitle(title).setContentText(body).setSmallIcon(R.drawable.ic_notification).setAutoCancel(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(channelId, "Default Channel", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
