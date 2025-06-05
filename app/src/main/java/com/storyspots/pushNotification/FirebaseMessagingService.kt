93b898454c4c1b2db4c84d8554b1b620a058b2c1
Jo-GC <jordana.guilbride.capela@student.nhlstenden.com>
2025-06-05 20:28:07 +0200

    Add: Notification connection base

--- app/src/main/java/com/storyspots/pushNotification/FirebaseMessagingService.kt
+++ app/src/main/java/com/storyspots/pushNotification/FirebaseMessagingService.kt
@@ -7,14 +7,23 @@ import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import com.google.firebase.messaging.RemoteMessage
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import com.storyspots.R
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class FirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage){
+        val title = remoteMessage.notification?.title ?: "No Title"
+        val body = remoteMessage.notification?.body ?: "No body"
+
+        saveNotificationToFirestore(title, body)
+
         remoteMessage.notification?.let {
             showNotification(it.title, it.body)
         }
@@ -38,4 +47,19 @@ class FirebaseMessagingService : FirebaseMessagingService() {
 
         notificationManager.notify(0, notificationBuilder.build())
     }
+
+    private fun saveNotificationToFirestore(title: String, body: String){
+        val notificationDoc = hashMapOf(
+
+            "title" to title,
+            "body" to body,
+            "timestamp" to Timestamp.now(),
+            "uid" to FirebaseAuth.getInstance().uid,
+            "read" to false
+        )
+
+        FirebaseFirestore.getInstance().collection("notification").add(notificationDoc).addOnFailureListener { e -> Log.e("FCM", "Failed to save notification", e)
+        }
+    }
+
 }
