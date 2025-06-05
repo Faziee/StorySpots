c65c03397cec52bfb475162a578bc45afbca3029
Sara Bubanova <sara.bubanova@student.nhlstenden.com>
2025-06-05 02:04:48 +0200

    added YourFeedScreen for logged in users stories

--- app/src/main/java/com/storyspots/yourFeed/YourFeedScreen.kt
+++ app/src/main/java/com/storyspots/yourFeed/YourFeedScreen.kt
@@ -1,6 +1,5 @@
-package com.storyspots.yourFeedAdd
+package com.storyspots.yourFeed
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
