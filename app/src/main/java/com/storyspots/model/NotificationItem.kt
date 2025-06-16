9f01ccc03585bcefccd3be6c1ba70f7ba242fd14
Faziee <144025566+Faziee@users.noreply.github.com>
2025-06-17 00:15:34 +0200

    updated the notification structure

--- app/src/main/java/com/storyspots/model/NotificationItem.kt
+++ app/src/main/java/com/storyspots/model/NotificationItem.kt
@@ -1,14 +1,26 @@
 package com.storyspots.model
-import java.util.Date
+
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.Timestamp
 
 data class NotificationItem(
     val id: String,
-    val createdAt: Date,
+    val created_at: Timestamp,
     val title: String,
     val message: String? = null,
-    val read: Boolean,
-    val fromUserId :String,
-    val story: String,
+    val authorId: String,
+    val story: DocumentReference,
     val imageUrl: String? = null
-)
-
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "id" to id,
+            "title" to title,
+            "message" to message,
+            "created_at" to created_at,
+            "authorId" to authorId,
+            "story" to story,
+            "imageUrl" to imageUrl
+        )
+    }
+}
