d4156bc783592a18102738f9fa753aa564fdd59e
Faziee <144025566+Faziee@users.noreply.github.com>
2025-06-20 20:28:26 +0200

    finalize App

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,20 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials {
+                username = "mapbox"
+                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").orNull
+            }
+
+        }
     }
 }
 
